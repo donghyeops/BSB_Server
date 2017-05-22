2cd5411231a6a7e6d8e3eb983f65c612fc5da4e2
ksw9446 <ksw9446@naver.com>
2017-05-22 13:01:05 +0900

    오류 수정
    어플이 예약할 때 메시지 한번 보냄
     -> 예약하기 전에 버스 정보 확인 메시지 먼저 보내고, 이후에 예약 메시지 추가로 보냄

--- src/server/ChildThread.java
+++ src/server/ChildThread.java
@@ -61,17 +61,27 @@ public class ChildThread implements Callable<Void> {
 			/* 받은 메시지의 타입에 따른 작업 수행*/
 			switch (type) {
 			/* 예약 메시지 */ // 테스트 완료
-			case MSG.Reservation_AtoS_2:
-				if (reservateGetOn(s_msg))
+			case MSG.Query_AtoS_2:
+				if (queryBus(s_msg))
+					Log.out("어플", "IN", app_socket, "BUS_ID:" + reservation.bus_id + " 버스 정보 확인");
+				else {
+					Log.err("어플", "IN", app_socket, "버스 정보 확인 실패");
+					CC.sendToApp(MSG.Failure_StoA_0);
+				}
+				break;
+				
+			/* 예약 메시지 */ // 테스트 완료
+			case MSG.Reservation_AtoS_4:
+				if (reservateGetOn())
 					Log.out("어플", "IN", app_socket, "BUS_ID:" + reservation.bus_id + " 버스 예약");
 				else {
-					Log.err("어플", "IN", app_socket, "예약 실패");
+					Log.err("어플", "IN", app_socket, "BUS_ID:" + reservation.bus_id + " 예약 실패");
 					CC.sendToApp(MSG.Failure_StoA_0);
 				}
 				break;
 				
 			/* 예약 취소 메시지 */ // 테스트 완료
-			case MSG.CancelReservation_AtoS_5:
+			case MSG.CancelReservation_AtoS_6:
 				if (cancelReservation()) {
 					Log.out("어플", "IN", app_socket, "BUS_ID:" + reservation.bus_id + " 예약 취소");
 					break SERVICE;
@@ -83,7 +93,7 @@ public class ChildThread implements Callable<Void> {
 				break;
 				
 			/* 탑승 메시지 */ // 테스트 완료
-			case MSG.GetOn_AtoS_7:
+			case MSG.GetOn_AtoS_8:
 				if (processGetOn(s_msg))
 					Log.out("어플", "IN", app_socket, "BUS_ID:" + reservation.bus_id + " 탑승 처리");
 				else
@@ -91,7 +101,7 @@ public class ChildThread implements Callable<Void> {
 				break;
 			
 			/* 하차 메시지 */ // 테스트 완료
-			case MSG.GetOffNext_StoB_10:
+			case MSG.GetOffNext_AtoS_10:
 				if (reservateGetOff())
 					Log.out("어플", "IN", app_socket, "BUS_ID:" + reservation.bus_id + " 하차 예약");
 				else
@@ -99,11 +109,11 @@ public class ChildThread implements Callable<Void> {
 				break;
 				
 			/* 하차 결과 메시지 */ // 테스트 완료
-			case MSG.GetOffResult_AtoS_11:
+			case MSG.GetOffResult_AtoS_12:
 				if (!processGetOff(s_msg))
 					Log.err("어플", "IN", app_socket, "BUS_ID:" + reservation.bus_id + " 하차 처리 실패");
 				if (END) {
-					System.out.println("[" + app_socket.getLocalAddress().toString() + "] 하차 완료");
+					Log.out("어플", "IN", app_socket, "BUS_ID:" + reservation.bus_id + " 하차 완료");
 					break SERVICE;
 				}
 				break;
@@ -121,36 +131,44 @@ public class ChildThread implements Callable<Void> {
 		return null;
 	}
 	
-	/** 탑승 예약 함수
-	 * <br>1. 버스와 연결 후, 버스에게 탑승 알림 메시지 전송(GetOnNext_StoB_3)
-	 * <br>2. 버스의 정보를 DB로부터 읽어서 어플에게 전송(NotifyOfBusBeacon_StoA_4)
+	/** 버스 정보 확인 함수
+	 * <br>1. 버스의 정보를 DB로부터 읽어서 어플에게 전송(NotifyOfBusBeacon_StoA_3)
 	 *  */
-	private boolean reservateGetOn(StringTokenizer s_msg) {
+	private boolean queryBus(StringTokenizer s_msg) {
 		reservation.inputReservation(s_msg); // 예약 정보 받아서 기록
-		if (!CC.connectToBus(reservation.bus_id)) // 버스 아두이노와 연결
-			return false;
-		
-		if (!CC.sendToBus(MSG.GetOnNext_StoB_3)) // 버스에게 탑승 알람 메시지 전송
-			return false;
 		
 		String[] beaconInfo = db.getBusBeacon(reservation.bus_id); // DB로부터 버스의 비콘 정보 부르기
 
 		if (beaconInfo == null)
 			return false;
-		reservation.isReservated = true; // 예약 처리
-		String msg = MSG.NotifyOfBusBeacon_StoA_4 + "-" + beaconInfo[0] + "-" + beaconInfo[1] + "-" + beaconInfo[2];
+		String msg = MSG.NotifyOfBusBeacon_StoA_3 + "-" + beaconInfo[0] + "-" + beaconInfo[1] + "-" + beaconInfo[2];
 		return CC.sendToApp(msg); // 어플에게 비콘 정보 보냄
 	}
 	
+	/** 탑승 예약 함수
+	 * <br>1. 버스와 연결 후, 버스에게 탑승 알림 메시지 전송(GetOnNext_StoB_5)
+	 * <br>2. 어플에게 성공 메시지 응답(Success_StoA_1)
+	 *  */
+	private boolean reservateGetOn() {
+		if (!CC.connectToBus(reservation.bus_id)) // 버스 아두이노와 연결
+			return false;
+		
+		if (!CC.sendToBus(MSG.GetOnNext_StoB_5)) // 버스에게 탑승 알람 메시지 전송
+			return false;
+		reservation.isReservated = true; // 예약 처리
+		
+		return CC.sendToApp(MSG.Success_StoA_1); // 성공 메시지 응답
+	}
+	
 	/** 예약 취소 함수
-	 * <br>1. 버스에게 예약 취소 메시지 전송(CancelReservation_StoB_6)
+	 * <br>1. 버스에게 예약 취소 메시지 전송(CancelReservation_StoB_7)
 	 * <br>2. 어플에게 성공 메시지 응답(Success_StoA_1)
 	 *  */
 	private boolean cancelReservation() {
 		if (!reservation.isReservated) // 예약이 안되있을 경우
 			return false;
 		
-		if (!CC.sendToBus(MSG.CancelReservation_StoB_6)) // 취소 메시지 전송
+		if (!CC.sendToBus(MSG.CancelReservation_StoB_7)) // 취소 메시지 전송
 			return false;
 		
 		END = true;
@@ -158,7 +176,7 @@ public class ChildThread implements Callable<Void> {
 	}
 	
 	/** 탑승 처리 함수 
-	 * <br>1. 버스에게 결제 결과 메시지 전송(PayResult_StoB_8)
+	 * <br>1. 버스에게 결제 결과 메시지 전송(PayResult_StoB_9)
 	 * <br>2. 어플에게 성공 메시지 전송(Success_StoA_1)
 	 * */
 	private boolean processGetOn(StringTokenizer s_msg) {
@@ -171,21 +189,21 @@ public class ChildThread implements Callable<Void> {
 		if (reservation.auto_pay == true) // 자동 결제 선택 시
 			result = PaySystem.pay(reservation); // 결제
 		
-		String msg = MSG.PayResult_StoB_8 + "-" + Integer.toString(result);
+		String msg = MSG.PayResult_StoB_9 + "-" + Integer.toString(result);
 		if (!CC.sendToBus(msg)) // 결제 결과 메시지를 버스 아두이노에게 보냄
 			return false;
 		return CC.sendToApp(MSG.Success_StoA_1); // 성공 메시지 응답
 	}
 	
 	/** 하차 예약 함수
-	 * <br>1. 버스에게 하차 예약 메시지 전송(GetOffNext_StoB_10)
+	 * <br>1. 버스에게 하차 예약 메시지 전송(GetOffNext_StoB_11)
 	 * <br>2. 어플에게 성공 메시지 전송(Success_StoA_1)
 	 *  */
 	private boolean reservateGetOff() {
 		if (!reservation.isReservated) // 예약이 안되있을 경우
 			return false;
 		
-		if (!CC.sendToBus(MSG.GetOffNext_StoB_10)) // 버스에게 하차 예약 메시지 전달
+		if (!CC.sendToBus(MSG.GetOffNext_StoB_11)) // 버스에게 하차 예약 메시지 전달
 			return false;
 		return CC.sendToApp(MSG.Success_StoA_1); // 성공 메시지 응답
 	}
--- src/server/MSG.java
+++ src/server/MSG.java
@@ -16,53 +16,58 @@ public interface MSG {
 	 *  */
 	int Success_StoA_1 = 1;
 	
-	/** 초기 예약 메시지(탑승하려는 버스ID, 출발정류장ID)
+	/** 버스 정보 확인 메시지(탑승하려는 버스ID, 출발정류장ID)
 	 *  <p> Example : "2-4575-41571"
 	 *  */
-	int Reservation_AtoS_2 = 2;
-	
-	/** 탑승 예정 알람 메시지
-	 *  <p> Example : "3"
-	 *  */
-	int GetOnNext_StoB_3 = 3;
+	int Query_AtoS_2 = 2;
 	
 	/** 버스의 비콘 정보 알림 (버스 앞문의 BeaconID, 버스 뒷문의 BeaconID, 비콘 사이의 거리(미터))
-	 *  <p> Example : "4-15648F-15648B-13.40"
+	 *  <p> Example : "3-15648F-15648B-13.40"
 	 *  */
-	int NotifyOfBusBeacon_StoA_4 = 4;
+	int NotifyOfBusBeacon_StoA_3 = 3;
 	
-	/** 예약 취소 메시지
+	/** 예약 메시지
+	 *  <p> Example : "4"
+	 *  */
+	int Reservation_AtoS_4 = 4;
+	
+	/** 탑승 예정 알람 메시지
 	 *  <p> Example : "5"
 	 *  */
-	int CancelReservation_AtoS_5 = 5;
+	int GetOnNext_StoB_5 = 5;
 	
 	/** 예약 취소 메시지
 	 *  <p> Example : "6"
 	 *  */
-	int CancelReservation_StoB_6 = 6;
+	int CancelReservation_AtoS_6 = 6;
+	
+	/** 예약 취소 메시지
+	 *  <p> Example : "7"
+	 *  */
+	int CancelReservation_StoB_7 = 7;
 	
 	/** 탑승 완료 메시지(선결제T/F) T면, (T, 카드번호, 유효기간, 도착정류장ID)
-	 *  <p> Example : "7-T-0101045445715584-05/21-56712" or "7-F"
+	 *  <p> Example : "8-T-0101045445715584-05/21-56712" or "8-F"
 	 *  */
-	int GetOn_AtoS_7 = 7;
+	int GetOn_AtoS_8 = 8;
 	
-	/** 결제 결과 메시지(성공:0, 실패:1, 선결제F:2). 선결제F면 직접 찍게 해야함
-	 *  <p> Example : "8-0" or "8-1" or "8-2"
+	/** 결제 결과 메시지(선결제F:0, 결제성공:1, 결제실패:2). 선결제F면 직접 찍게 해야함
+	 *  <p> Example : "9-0" or "9-1" or "9-2"
 	 *  */
-	int PayResult_StoB_8 = 8;
+	int PayResult_StoB_9 = 9;
 	
 	/** 하차 예정 알람 메시지 [사용자->서버]
-	 *  <p> Example : "9"
-	 *  */
-	int GetOffNext_AtoS_9 = 9;
-	
-	/** 하차 예정 알람 메시지 [서버->버스]
 	 *  <p> Example : "10"
 	 *  */
-	int GetOffNext_StoB_10 = 10;
+	int GetOffNext_AtoS_10 = 10;
+	
+	/** 하차 예정 알람 메시지 [서버->버스]
+	 *  <p> Example : "11"
+	 *  */
+	int GetOffNext_StoB_11 = 11;
 	
 	/** 하차 성공 여부(T/F)
-	 *  <p> Example : "11-T" or "11-F"
+	 *  <p> Example : "12-T" or "12-F"
 	 *  */
-	int GetOffResult_AtoS_11 = 11;
+	int GetOffResult_AtoS_12 = 12;
 }
