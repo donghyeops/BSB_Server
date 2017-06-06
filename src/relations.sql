810c1484ce46a9c13e90730fd8777812f36ca701
ksw9446 <ksw9446@naver.com>
2017-06-06 15:29:56 +0900

    db data 수정
    - 5000번 버스 / 실제 비콘 ID 삽입

--- src/relations.sql
+++ src/relations.sql
@@ -3,14 +3,14 @@ drop table bus;
 
 create table bus(
 	bus_id varchar(45) not null primary key,
-	f_beacon varchar(45) unique, -- 앞문의 비콘 아이디
-	b_beacon varchar(45) unique, -- 뒷문의 비콘 아이디
+	f_beacon varchar(45), -- 앞문의 비콘 아이디
+	b_beacon varchar(45), -- 뒷문의 비콘 아이디
 	distance float, -- 비콘 사이의 거리
-	ip varchar(20) unique, -- 버스 아두이노의 IP
+	ip varchar(20), -- 버스 아두이노의 IP
 	port int -- 버스 아두이노의 port
 );
 
 create table stop(
 	stop_n int not null primary key,
-	beacon varchar(45) unique
+	beacon varchar(45)
 );
